c298e097acf26fdd37f7b04560eb0c007120302c
juniorrobles31-blip <junior.robles31@gmail.com>
2026-06-08 11:39:13 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,57 +1,12 @@
-<?php
-ob_start( );
-
-
-require_once("include/lib/variables.lib.php");
-require_once("include/lib/functions.lib.php");
-//include("include/lib/setting.lib.php");
-
-
-
-
-$page = ""; if(isset($_GET['page'])){$page =$_GET['page'];}
-$page = str_replace(".","",$page);// remueve el punto para evitar que se muevan a carpetas superiores
-////-----------------------------------------------------
-//// check session login
-////-----------------------------------------------------
-sec_session_start();
-	
-//if(isset($_SESSION['ID'])&&isset($_SESSION['role'])){
-					
-////-----------------------------------------------------
-//// session time
-////-----------------------------------------------------
-	/*
-	if ($_SESSION['expireSession'] < time()-$timeout){	
-		echo "<script>alert('La session ha expirado222');</script>";
-		$urlLogout = true;		
-	}else{
-		$_SESSION['expireSession'] = time();
-		echo "<script>alert('Actualizo: ".$_SESSION['expireSession']."');</script>";
-	}*/	
-
-	/*if(isset($urlLogout)){
-		$file = "include/gui/logout.inc.php";
-	}elseif(!empty($page)){
-		if ( in_array($_SESSION['role'], array('ADM','ADS'), true ) ) {
-			if($_GET['page']=='logout'){
-				$file = "include/gui/logout.inc.php";
-			}
-		}else{
-			$file = "include/gui/".$page.".inc.php";
-		}
+<style>
+	body{
+		margin: 0px;
+		background-color: #4d4d4d;
 	}
-}else{
-  $file = "include/gui/login.inc.php";
-}*/
-$file = "include/gui/".$page.".inc.php";// Remover cuando este listo
-require_once("include/menu/header.php");
-$CtrlPage = true;
+	.frame{
+		border: none;
+		position:fixed;
+	}
+</style>
 
-if(file_exists($file)){ include_once($file); }
-
-require_once 'include/menu/footer.php'; 
-
-
-ob_end_flush( );
-?>
+<iframe id="frame_tv" width="100%" height="100%" class="frame" src="../tv.php" frameborder="0" scrolling="no"></iframe>
